8417be543b12359b2a324d519921b7bbec86598c
mar-robba <marcorobba03@gmail.com>
2025-01-11 15:38:29 +0100

    sos

--- miorest/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module upo20052959.ristorante.miorest {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens upo20052959.ristorante.miorest to javafx.fxml;
-    exports upo20052959.ristorante.miorest;
-}
