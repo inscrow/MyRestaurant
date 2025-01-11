cdfaabff61570d5a3fee17709f1826d42b4e7ffa
mar-robba <marcorobba03@gmail.com>
2025-01-11 15:39:19 +0100

    sos

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module upo20052959.ristorante.miorest {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens upo20052959.ristorante.miorest to javafx.fxml;
+    exports upo20052959.ristorante.miorest;
+}
---
+++ src/main/java/upo20052959/ristorante/miorest/HelloController.java
@@ -0,0 +1,14 @@
+package upo20052959.ristorante.miorest;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
